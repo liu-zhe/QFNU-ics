6d9561c39e48e16b69bdf5fa832c228d942d8f21
lukzia <31232941+liu-zhe@users.noreply.github.com>
2023-08-18 16:57:03 +0800

    Add files via upload
    优化了登陆的方式

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import requests
+# -*- coding: utf-8 -*-
+
 import ics
 import qfnu_api
-import browser_cookie3
-import requests
 import datetime as dt
-from time import sleep
 
 
 cal = ics.Calendar()
@@ -35,7 +33,9 @@ class_time_end = {
     "09": "19:50:00",
     "10": "20:50:00",
     "11": "21:50:00",
-    "12": "21:50:00"  # 哪个脑瘫犯的错误，12节课
+    "12": "21:50:00",  # 哪个脑瘫犯的错误，12节课
+    "13": "21:50:00",
+    "14": "21:50:00"
 }
 
 summer_class_time_begin = {
@@ -64,55 +64,54 @@ summer_class_time_end = {
     "09": "19:50:00",
     "10": "20:50:00",
     "11": "21:50:00",
-    "12": "21:50:00"  # 哪个脑瘫犯的错误，12节课
+    "12": "21:50:00",  # 哪个脑瘫犯的错误，12节课
+    "13": "21:50:00",
+    "14": "21:50:00"
 }
 
 duration_per_class = 45
 
+userAccount = input("输入账号：")
+userPassword = input("输入密码：")
 
-sjmsValue = input("打开浏览器登录曲阜师范大学教务系统\n请输入您的sjmsValue（不输入则使用默认）：")
-print(sjmsValue)
-
-cookie = browser_cookie3.load('202.194.188.38')
-cookies = requests.utils.dict_from_cookiejar(cookie)
-api = qfnu_api.QFNUApi(cookies)
+account = qfnu_api.QFNUApi(userAccount, userPassword)
+account.display_random_code()
+account.get_encoded()
+account.login()
 
 print("Sending POST request and generating ics...")
 
 now_date = dt.datetime.now()
 for i in range(1, 366, 1):
-    # print(now_date.strftime("%a"));
-    if (sjmsValue == "\n"):
-        api.fetch(now_date)
-    else:
-        api.fetch(now_date, sjmsValue)
+    # print(now_date.strftime("%a"))
+    account.fetch(now_date)
 
     vis = []
-    for j in range(0, len(api.name)):
-        if (api.name[j] not in vis):
+    for j in range(0, len(account.name)):
+        if (account.name[j] not in vis):
             e = ics.Event()
-            e.name = api.name[j]
-            e.location = api.location[j]
-            e.description = api.info[j]
+            e.name = account.name[j]
+            e.location = account.location[j]
+            e.description = account.info[j]
 
             # 夏季作息时间特判
             if (now_date > dt.datetime(dt.datetime.now().year, 5, 1) and now_date < dt.datetime(dt.datetime.now().year, 10, 1)):
                 date_time = dt.datetime.strptime(now_date.strftime("%Y-%m-%d") +
-                                                 " " + summer_class_time_begin[api.begin[j]], '%Y-%m-%d %H:%M:%S')
+                                                 " " + summer_class_time_begin[account.begin[j]], '%Y-%m-%d %H:%M:%S')
                 e.begin = str(date_time - dt.timedelta(hours=8))
                 date_time = dt.datetime.strptime(now_date.strftime("%Y-%m-%d") +
-                                                 " " + summer_class_time_end[api.end[j]], '%Y-%m-%d %H:%M:%S')
+                                                 " " + summer_class_time_end[account.end[j]], '%Y-%m-%d %H:%M:%S')
                 e.end = str(date_time - dt.timedelta(hours=8))  # 时区转换
             else:
                 date_time = dt.datetime.strptime(now_date.strftime("%Y-%m-%d") +
-                                                 " " + class_time_begin[api.begin[j]], '%Y-%m-%d %H:%M:%S')
+                                                 " " + class_time_begin[account.begin[j]], '%Y-%m-%d %H:%M:%S')
                 e.begin = str(date_time - dt.timedelta(hours=8))
                 date_time = dt.datetime.strptime(now_date.strftime("%Y-%m-%d") +
-                                                 " " + class_time_end[api.end[j]], '%Y-%m-%d %H:%M:%S')
+                                                 " " + class_time_end[account.end[j]], '%Y-%m-%d %H:%M:%S')
                 e.end = str(date_time - dt.timedelta(hours=8))  # 时区转换
 
             cal.events.add(e)
-            vis.append(api.name[j])
+            vis.append(account.name[j])
     now_date = now_date + dt.timedelta(days=1)
 
 with open('output.ics', 'wb') as f:
--- qfnu_api.py
+++ qfnu_api.py
@@ -1,60 +1,158 @@
+# -*- coding: utf-8 -*-
+
 from bs4 import BeautifulSoup
 import requests
-from time import sleep
+from PIL import Image
+from io import BytesIO
 
 week = {
     "星期一": "Mon", "星期二": "Tue", "星期三": "Wed", "星期四": "Thu", "星期五": "Fri", "星期六": "Sat", "星期七": "Sun"
 }
+session = requests.session()
 
 
 class QFNUApi:
-    def __init__(this, cookies):
-        this.name = []
-        this.info = []
-        this.begin = []
-        this.end = []
-        this.location = []
-        this.url = "http://202.194.188.38/jsxsd/framework/main_index_loadkb.jsp"
-        this.headers = {
-            "Host": "202.194.188.38",
-            "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.50",
-            "Origin": "http://202.194.188.38",
-            "Referer": "http://202.194.188.38/jsxsd/framework/xsMain_new.jsp?t1=1"
-        }
-        this.cookies = cookies
+    RandCodeUrl = 'http://zhjw.qfnu.edu.cn/verifycode.servlet'
+    loginUrl = 'http://zhjw.qfnu.edu.cn/Logon.do?method=logonLdap'
+    dataStrUrl = 'http://zhjw.qfnu.edu.cn/Logon.do?method=logon&flag=sess'
+    kbUrl = "http://zhjw.qfnu.edu.cn/jsxsd/framework/main_index_loadkb.jsp"
 
-    def fetch(this, _date, sjmsValue='94786EE0ABE2D3B2E0531E64A8C09931'):
-        this.name.clear()
-        this.info.clear()
-        this.location.clear()
-        this.begin.clear()
-        this.end.clear()
+    kbWeek = -1
+    kbText = ""
+    num = 0
+
+    def __init__(self, userAccount, userPassword):
+        self.name = []
+        self.info = []
+        self.begin = []
+        self.end = []
+        self.location = []
+        self.data = {
+            'userAccount': userAccount,
+            'userPassword':  userPassword,
+            "RANDOMCODE": '',
+            'encoded': ''
+        }
+
+        header = {
+            "Content-Type": "text/html;charset=utf-8",
+            "Vary": "Accept-Encoding"
+        }
+        response = session.get(
+            url=self.dataStrUrl, headers=header, timeout=1000)
+        self.cookies = session.cookies.get_dict()
+        self.dataStr = response.text
+
+    def display_random_code(self):
+        response = session.get(
+            self.RandCodeUrl, cookies=self.cookies)
+        image = Image.open(BytesIO(response.content))
+        image.show()
+        self.data['RANDOMCODE'] = input("输入验证码内容：")
+
+    def get_encoded(self):
+        res = self.dataStr.split("#")
+        code = res[0]
+        sxh = res[1]
+        data = self.data['userAccount'] + "%%%" + self.data['userPassword']
+        length = 0
+        for b in code:
+            length += 1
+        encoded = ""
+        b = 0
+        for a in range(length):
+            if a < 20:
+                encoded = encoded + data[a]
+                for c in range(int(sxh[a])):
+                    encoded = encoded + code[b]
+                    b += 1
+            else:
+                encoded = encoded + data[a:]
+                self.data['encoded'] = encoded
+                return encoded
+            a += 1
+        self.data['encoded'] = encoded
+        return encoded
+
+    def login(self):
+        print(self.data)
+        header = {
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;"
+                      "q=0.8,application/signed-exchange;v=b3;q=0.9",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "zh-CN,zh;q=0.9",
+            "Content-Length": "135",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Host": "zhjw.qfnu.edu.cn",
+            "Origin": "http://zhjw.qfnu.edu.cn",
+            "Proxy-Connection": "keep-alive",
+            "Referer": "http://zhjw.qfnu.edu.cn/",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                          " Chrome/83.0.4103.116 Safari/537.36",
+        }
+        response = session.post(
+            url=self.loginUrl, headers=header, data=self.data, cookies=self.cookies, timeout=1000)
+
+        # print(response.text)
+        return self.cookies
+
+    def fetch(self, _date):
+        self.name.clear()
+        self.info.clear()
+        self.location.clear()
+        self.begin.clear()
+        self.end.clear()
         date = _date.strftime("%Y-%m-%d")
-        res = requests.post(
-            this.url,
-            headers=this.headers,
-            data={'rq': date, 'sjmsValue': sjmsValue},
-            cookies=this.cookies
-        )
-        soup = BeautifulSoup(res.text, 'html.parser')
-        data = soup.select("p")
+        # 将日期字符串转换为datetime对象
+
+        # 计算日期的周数
+        _week = _date.isocalendar()[1]
+
+        # 判断周数是否相等
+        if _week != self.kbWeek:
+            self.kbWeek = _week
+            header = {
+                "Host": "zhjw.qfnu.edu.cn",
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.50",
+                "Origin": "http://zhjw.qfnu.edu.cn",
+                "Referer": "http://zhjw.qfnu.edu.cn/jsxsd/framework/xsMain_new.jsp?t1=1"
+            }
+            res = session.post(
+                self.kbUrl,
+                headers=header,
+                data={'rq': date, 'sjmsValue': '94786EE0ABE2D3B2E0531E64A8C09931'},
+                cookies=self.cookies,
+                timeout=3000
+            )
+            soup = BeautifulSoup(res.text, 'html.parser')
+            # print(soup)
+            data = soup.select("p")
+            self.kbText = data
+            self.num += 1
+            print('POST #' + str(self.num))
+        else:
+            data = self.kbText
+
         for i in range(0, len(data), 1):
             htmlstr = str(data[i])
             htmlcontent = htmlstr[70:len(htmlstr)]
             content = htmlcontent.split("&lt;br/&gt;")
-
+            # print(content)
             if (week[content[3].split(" ")[1]] == _date.strftime("%a")):
-                this.name.append(content[2])
-                this.info.append(content[0] + content[1])
-                this.location.append(content[4])
+                self.name.append(content[2])
+                self.info.append(content[0] + content[1])
+                self.location.append(content[4])
 
                 class_time = content[3].split(" ")[2].replace('[', '')
                 class_time = class_time.replace(']节', '')
-                this.begin.append(class_time.split("-")[0])
-                this.end.append(class_time.split("-").pop())
+                self.begin.append(class_time.split("-")[0])
+                self.end.append(class_time.split("-").pop())
 
-            # content[len(content)-1] = content[len(content)-1].replace('</p>', '')
-            # content[len(content)-1] = content[len(content)-1].replace('\">', ' ')
+            # content[len(content)-1] = content[len(content) -
+            #                                   1].replace('</p>', '')
+            # content[len(content)-1] = content[len(content) -
+            #                                   1].replace('\">', ' ')
             # for i in range(0, len(content), 1):
             #     print(content[i])
